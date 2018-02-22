605ee6a87c911a3e8a6a5df392f3b52ae7f6b3ce
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-02-22 09:51:40 +0000

    Create utilities.cc

---
+++ dev/src/inclusion/utilities.cc
@@ -0,0 +1,25 @@
+template<typename Out>
+void split (const std::string &s, char delim, Out result) {
+	std::stringstream ss(s);
+	std::string item;
+	while (std::getline(ss,item,delim)) {
+		*(result++) = item;
+	}
+}
+
+std::vector<std::string> split (const std::string &s, char delim) {
+	std::vector<std::string> elems;
+	split(s, delim, std::back_inserter(elems));
+	return elems;
+}
+
+void delay (int x) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(x));
+}
+
+int toInt (string in) {
+	int val = 0;
+	stringstream converter(in);
+	converter >> val;
+	return val;
+}
