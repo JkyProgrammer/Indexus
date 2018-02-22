6652da518f4d66571cccef3b882dac65b1487839
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-02-22 09:50:30 +0000

    Update core.cc

--- dev/src/core.cc
+++ dev/src/core.cc
@@ -17,32 +17,6 @@ vector<Node*> narray;
 int startPointer = 1;
 std::vector<std::string> memory;
 
-template<typename Out>
-void split (const std::string &s, char delim, Out result) {
-	std::stringstream ss(s);
-	std::string item;
-	while (std::getline(ss,item,delim)) {
-		*(result++) = item;
-	}
-}
-
-std::vector<std::string> split (const std::string &s, char delim) {
-	std::vector<std::string> elems;
-	split(s, delim, std::back_inserter(elems));
-	return elems;
-}
-
-void delay (int x) {
-	std::this_thread::sleep_for(std::chrono::milliseconds(x));
-}
-
-int toInt (string in) {
-	int val = 0;
-	stringstream converter(in);
-	converter >> val;
-	return val;
-}
-
 void interpret (string instruction) {
 	std::vector<string> splitInstr = split (instruction, '~');
 	string instr = splitInstr.at(0);
