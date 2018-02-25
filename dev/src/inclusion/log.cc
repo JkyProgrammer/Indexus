8a3e63529c376a0003a55f119da7372badb6dc88
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-02-25 22:37:51 +0000

    Create log.cc

---
+++ dev/src/inclusion/log.cc
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string>
+#include <iostream>
+#include <pthread.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <sstream>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <array>
+#include <iomanip>
+#include <fstream>
+
+void log (string tx) {}
