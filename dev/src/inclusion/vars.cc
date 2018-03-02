d93c701f1dae122be8bbca68dbd1b265cbbc48d5
Jake Costen <32058792+SketchesJavax@users.noreply.github.com>
2018-03-02 12:54:27 +0000

    Wrote functions for var control

--- dev/src/inclusion/vars.cc
+++ dev/src/inclusion/vars.cc
@@ -10,3 +10,15 @@
 #include <iterator>
 
 std::map<string, string> vars;
+
+void setValue (string key, string value) {
+  vars.insert (std::pair<string,string>(key, value));
+}
+
+string getValue (string key) {
+  return vars.at (key);
+}
+
+void removeKeyValPair (string key) {
+  vars.erase (key);
+}
